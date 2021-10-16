ad7303cd3ef855fb52808d37342d6450ce062dc2
ko1596 <ko89022189@gmail.com>
2021-10-16 21:50:09 +0800

    add customer bootstrap scss

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/logo.png" />
+    <router-view />
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
+    <a href="#">aaaa</a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   created() {
     const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
     //"https://vue-course-api.hexschool.io/api/order/products"
-    console.log(process.env.APIPATH, process.env.CUSTOMPATH)
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH);
     this.axios.get(api).then((response) => {
       console.log(response.data);
-    })
+    });
   },
-}
+};
 </script>
 
 <style lang="scss">
-@import "~bootstrap/scss/bootstrap";
+@import "./assets/all";
 </style>
