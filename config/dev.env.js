9924c8e475935e39fc461c3ed950d85dd2a3ac5f
ko1596 <ko89022189@gmail.com>
2021-10-16 21:07:20 +0800

    新增ajax環境變數

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io"',
+  CUSTOMPATH: '"order"',
 })
--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,15 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    //"https://vue-course-api.hexschool.io/api/order/products"
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH)
+    this.axios.get(api).then((response) => {
+      console.log(response.data);
+    })
+  },
 }
 </script>
 
